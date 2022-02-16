b762be7ce639c980b6b9fd050bf641b705e6b03f
NoySouliya <souliyatmv24@gmail.com>
2022-02-16 16:09:18 +0700

    update

--- FaceDetector2.py
+++ FaceDetector2.py
@@ -1,8 +1,16 @@
+import tkinter as tk
+from tkinter import *
 import cv2
-import numpy as np
 import pymysql
-import winsound
-from PIL import Image
+import time
+
+window = tk.Tk()
+window.title("FAMS-Face Recognition Based Attendance Management System")
+
+window.geometry('1280x720')
+window.configure(background='snow')
+
+
 
 faceDetect = cv2.CascadeClassifier('Detect/haarcascade_frontalface_default.xml')
 cam = cv2.VideoCapture(0)
@@ -13,33 +21,57 @@ fontface=cv2.FONT_HERSHEY_SIMPLEX
 fontScale = 2
 fontColor = (255,0,0)
 
-def getProfile(Id):
-    connection = pymysql.connect(host="localhost", user="root", password="", database="facebase")
-    conn = connection.cursor()
-    sql = "SELECT * FROM people where P_ID='"+str(Id)+"';"
-    conn.execute(sql)
-    profile=None
-    for row in conn:
-        profile=row
-    conn.close()
-    return profile
+def auto():
+    def getProfile(Id):
+        connection = pymysql.connect(host="localhost", user="root", password="", database="facebase")
+        conn = connection.cursor()
+        sql = "SELECT * FROM people where P_ID='"+str(Id)+"';"
+        conn.execute(sql)
+        profile=None
+        for row in conn:
+            profile=row
+        conn.close()
+        return profile
 
-while(True):
-    ret, img = cam.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = faceDetect.detectMultiScale(gray, 1.3, 5)
-    for(x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        Id, conf = rec.predict(gray[y:y+h, x:x+w])
-        if(conf<50):
-            profile = getProfile(Id)
-            if(profile!=None):
-                cv2.putText(img, str(profile[0]), (x, y+h+30), fontface, fontScale, fontColor)
-                cv2.putText(img, str(profile[1]), (x, y+h+80), fontface, fontScale, fontColor)
-        else:
-            cv2.putText(img, "Unknown", (x, y+h+30), fontface, fontScale, fontColor)
-    cv2.imshow("Face", img)
-    if(cv2.waitKey(1) & 0xFF == ord('q')):
-        break
-cam.release()
-cv2.destroyAllWindows()
+    while(True):
+        ret, img = cam.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = faceDetect.detectMultiScale(gray, 1.3, 5)
+        for(x, y, w, h) in faces:
+            now = time.time()
+            future = now + 20
+            Id, conf = rec.predict(gray[y:y+h, x:x+w])
+            if(conf<70):
+                global profile
+                profile = getProfile(Id)
+                if(profile!=None):
+                    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                    cv2.putText(img, str(profile[0]), (x, y+h+30), fontface, fontScale, fontColor)
+                    cv2.putText(img, str(profile[1]), (x, y+h+80), fontface, fontScale, fontColor)
+            else:
+                cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                cv2.putText(img, "Unknown", (x, y+h+30), fontface, fontScale, fontColor)
+        cv2.imshow("Face", img)
+        key = cv2.waitKey(1) & 0xFF == ord('q')
+        if key:
+            break
+    try:
+        connection = pymysql.connect(host="localhost", user="root", password="", database="facebase")
+        conn = connection.cursor()
+    except Exception as e:
+        print(e)
+
+    insert_data =  "INSERT INTO attandance VALUES (0, %s, %s)"
+    VALUES = (str(profile[0]),str(profile[1]))
+    try:
+        conn.execute(insert_data, VALUES)
+        connection.commit()
+    except Exception as ex:
+        print(ex)
+    cam.release()
+    cv2.destroyAllWindows()
+
+
+FA = tk.Button(window, text="Automatic Attendace",fg="white",command=auto  ,bg="blue2"  ,width=20  ,height=3, activebackground = "Red" ,font=('times', 15, ' bold '))
+FA.place(x=690, y=500)
+window.mainloop()
--- FaceRecog.py
+++ FaceRecog.py
@@ -1,12 +1,14 @@
 from os import name
 import cv2
 import numpy as np
+import pandas as pd
 import pymysql
 
 faceDetect = cv2.CascadeClassifier('Detect/haarcascade_frontalface_default.xml')
 cam = cv2.VideoCapture(0)
 
 def insertOrUpdate(Id, Name):
+    global conn
     connection = pymysql.connect(host="localhost", user="root", password="", database="facebase")
     conn = connection.cursor()
     sql = "Select * from people where P_ID='"+str(Id)+"';"
@@ -21,10 +23,9 @@ def insertOrUpdate(Id, Name):
     conn.execute(sql)
     connection.commit()
     conn.close()
-
-id = input("Enter user ID:")
-name = input("Enter your name:")
-insertOrUpdate(id, name)
+Id = input("Enter user ID:")
+Name = input("Enter your name:")
+insertOrUpdate(Id, Name)
 SampleNum = 0
 while(True):
     ret, img = cam.read()
@@ -32,12 +33,12 @@ while(True):
     faces = faceDetect.detectMultiScale(gray, 1.3, 5)
     for(x, y, w, h) in faces:
         SampleNum=SampleNum+1
-        cv2.imwrite("DataSet/User."+str(id)+"."+str(SampleNum)+".jpg", gray[y:y+h, x:x+w])
+        cv2.imwrite("DataSet/ "+Name+"."+str(Id)+"."+str(SampleNum)+".jpg", gray[y:y+h, x:x+w])
         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
         cv2.waitKey(100)
     cv2.imshow("Face", img)
     cv2.waitKey(1)
-    if(SampleNum>50):
+    if(SampleNum>70):
         break
     
 cam.release()
